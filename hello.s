49758136e6e30dbac017e5d79f040610c2b5a1de
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-11-07 13:47:46 +0100

    style: formatting

--- hello.s
+++ hello.s
@@ -7,21 +7,20 @@
                .data
 login:         .asciiz        "Hello world!"           ; sem doplnte vas login
 cipher:        .space         17                       ; misto pro zapis sifrovaneho loginu
-
 params_sys5:   .space         8                        ; misto pro ulozeni adresy pocatku
                                                        ; retezce pro vypis pomoci syscall 5
                                                        ; (viz nize "funkce" print_string)
 
 ; CODE SEGMENT
                .text
-main:          daddi          r4, r0, login            ; vzorovy vypis: adresa login: do r4
-               jal            print_string             ; vypis pomoci print_string - viz nize
+main:          DADDI          r4, r0, login            ; vzorovy vypis: adresa login: do r4
+               JAL            print_string             ; vypis pomoci print_string - viz nize
 
 
-               syscall        0                        ; halt
+               SYSCALL        0                        ; halt
 
 print_string:                                          ; adresa retezce se ocekava v r4
-               sw             r4, params_sys5(r0)
-               daddi          r14, r0, params_sys5     ; adr pro syscall 5 musi do r14
-               syscall        5                        ; systemova procedura - vypis retezce na terminal
-               jr             r31                      ; return - r31 je urcen na return address
+               SW             r4, params_sys5(r0)
+               DADDI          r14, r0, params_sys5     ; adr pro syscall 5 musi do r14
+               SYSCALL        5                        ; systemova procedura - vypis retezce na terminal
+               JR             r31                      ; return - r31 je urcen na return address
