08a649a3191e24f74809c30d008cc9bebd850539
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-11-09 16:08:14 +0100

    style: rename `hello.s` to `vernam.s`

--- vernam.s
+++ vernam.s
@@ -5,7 +5,7 @@
 
 ; DATA SEGMENT
                .data
-login:         .asciiz        "Hello world!"           ; sem doplnte vas login
+login:         .asciiz        "xkrame00"               ; sem doplnte vas login
 cipher:        .space         17                       ; misto pro zapis sifrovaneho loginu
 params_sys5:   .space         8                        ; misto pro ulozeni adresy pocatku
                                                        ; retezce pro vypis pomoci syscall 5
@@ -13,14 +13,14 @@ params_sys5:   .space         8                        ; misto pro ulozeni adres
 
 ; CODE SEGMENT
                .text
-main:          DADDI          r4, r0, login            ; vzorovy vypis: adresa login: do r4
+main:          DADDI          R4, R0, login            ; vzorovy vypis: adresa login: do r4
                JAL            print_string             ; vypis pomoci print_string - viz nize
 
 
                SYSCALL        0                        ; halt
 
 print_string:                                          ; adresa retezce se ocekava v r4
-               SW             r4, params_sys5(r0)
-               DADDI          r14, r0, params_sys5     ; adr pro syscall 5 musi do r14
+               SW             R4, params_sys5(r0)
+               DADDI          R14, R0, params_sys5     ; adr pro syscall 5 musi do r14
                SYSCALL        5                        ; systemova procedura - vypis retezce na terminal
-               JR             r31                      ; return - r31 je urcen na return address
+               JR             R31                      ; return - r31 je urcen na return address
