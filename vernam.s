f6e3afdcd7c37b52f3e5e72f97a72e8c6c830357
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-11-09 17:54:24 +0100

    feat: create `encryptLoop` (currently gets keyVal)

--- vernam.s
+++ vernam.s
@@ -3,24 +3,40 @@
 ; Projekt 2 - INP 2022
 ; Vernamova sifra na architekture MIPS64
 
+; Moje registre
+; R0  ($zero) - nulova konstanta
+; R1  ($at)   - unsafe temp.      - pomocny register
+; R4  ($a0)   - function arg.     - ukazatel na retazec
+; R10 ($t2)   - temp. reg.        - pomocny register
+; R20 ($s4)   - saved reg.        - index v retazci (pocitadlo)
+; R29 ($sp)   - stack pointer
+
+
 ; DATA SEGMENT
-               .data
-login:         .asciiz        "xkrame00"               ; sem doplnte vas login
-cipher:        .space         17                       ; misto pro zapis sifrovaneho loginu
-params_sys5:   .space         8                        ; misto pro ulozeni adresy pocatku
-                                                       ; retezce pro vypis pomoci syscall 5
-                                                       ; (viz nize "funkce" print_string)
+                .data
+login:          .asciiz         "xkrame00"                  ; login
+key:            .asciiz         "on"                        ; sifrovaci kluc
+cipher:         .space          17                          ; miesto pre sifrovany login
+params_sys5:    .space          8                           ; miesto pre adresu pociatku retazca
 
 ; CODE SEGMENT
-               .text
-main:          DADDI          R4, R0, login            ; vzorovy vypis: adresa login: do r4
-               JAL            print_string             ; vypis pomoci print_string - viz nize
+                .text
+main:
+                DADDI           R4, R0, login               ; R4 = adresa retazca na sifrovanie
+                DADDI           R20, R0, 0                  ; R20 = index v retazci (pocitadlo)
 
+encryptLoop:
+                ANDI            R1, R20, 1                  ; R1 = 0 ak je index parny, inak R1 = 1
+                DADDI           R10, R0, key                ; R10 = adresa kluca
+                ADD             R1, R1, R10
+                LB              R10, 0(R1)                  ; R10 = znak kluca na pozicii R1
 
-               SYSCALL        0                        ; halt
+end:
+                JAL             print_string
+                SYSCALL         0
 
-print_string:                                          ; adresa retezce se ocekava v r4
-               SW             R4, params_sys5(r0)
-               DADDI          R14, R0, params_sys5     ; adr pro syscall 5 musi do r14
-               SYSCALL        5                        ; systemova procedura - vypis retezce na terminal
-               JR             R31                      ; return - r31 je urcen na return address
+print_string:
+                SW              R4, params_sys5(r0)
+                DADDI           R14, R0, params_sys5
+                SYSCALL         5
+                JR              R31
