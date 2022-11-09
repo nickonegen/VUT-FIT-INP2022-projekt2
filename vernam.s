94defaa5b49f8171760ba73d85fd799f2413ef52
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-11-09 23:27:39 +0100

    style: formatting

--- vernam.s
+++ vernam.s
@@ -3,21 +3,11 @@
 ; Projekt 2 - INP 2022
 ; Vernamova sifra na architekture MIPS64
 
-; Moje registre
-;  R0  ($zero) - nulova konstanta
-;  R1  ($at)   - assembler temp.
-;  R4  ($a0)   - function arg.
-;  R10 ($t2)   - temporary reg.
-;  R20 ($s4)   - saved reg.
-;  R29 ($sp)   - stack pointer
-
-; DATA SEGMENT
                 .data
 login:          .asciiz         "xkrame00"                  ; login
 cipher:         .space          17                          ; miesto pre sifrovany login
 params_sys5:    .space          8                           ; miesto pre adresu pociatku retazca
 
-; CODE SEGMENT
                 .text
 main:           DADDI           R20, R0, 0                  ; R20 = index v retazci (pocitadlo)
 encryptLoop:
@@ -68,7 +58,7 @@ keyIfEnd:       ; Zasifrovanie znaku
                 SB              R10, 0(R4)                  ; vlozenie sifrovaneho znaku do vystupneho retazca
 
                 ; Pokracovanie cyklu
-                DADDI           R20, R20, 1                 ; R20 = index + 1
+                DADDI           R20, R20, 1                 ; R20 = index++
                 B               encryptLoop
 
 encryptEnd:     ; Vypis vystupneho retazca
@@ -77,7 +67,7 @@ encryptEnd:     ; Vypis vystupneho retazca
                 SYSCALL         0
 
 print_string:
-                SW              R4, params_sys5(r0)
+                SW              R4, params_sys5(R0)
                 DADDI           R14, R0, params_sys5
                 SYSCALL         5
                 JR              R31
