44d1e318b7069481c742b537e84ab57a8cda3bfa
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-11-16 01:03:59 +0100

    perf: reduce 64b operations and memory loads

--- vernam.s
+++ vernam.s
@@ -9,7 +9,7 @@ cipher:         .space          17                          ; miesto pre sifrova
 params_sys5:    .space          8                           ; miesto pre adresu pociatku retazca
 
                 .text
-main:           DADDI           R20, R0, 0                  ; R20 = index v retazci (pocitadlo)
+main:           ADDU            R20, R0, R0                 ; R20 = index v retazci (pocitadlo)
 encryptLoop:    ; Ulozenie znaku na aktualnom indexe
                 LB              R10, login(R20)             ; R10 = ASCII znak na aktualnej pozicii
 
@@ -23,31 +23,32 @@ encryptLoop:    ; Ulozenie znaku na aktualnom indexe
                 ANDI            R1, R20, 1                  ; R1 = 0 ak je index parny, inak R1 = 1
                 BNEZ            R1, keyIfOdd
                 ; Parny index - kluc je 'o' (ASCII 111), posun dopredu (+)
-                DADDI           R4, R0, 111
-                DADDI           R4, R4, -96                 ; R4 = hodnota kluca
+                ADDI            R4, R0, 111
+                ADDI            R4, R4, -96                 ; R4 = hodnota kluca
                 B               keyIfEnd
 keyIfOdd:       ; Neparny index - kluc je 'n' (ASCII 110), posun dozadu (-)
-                DADDI           R4, R0, 110
-                DADDI           R4, R4, -96
-                DSUB            R4, R0, R4                  ; R4 = hodnota kluca
+                ADDI            R4, R0, 110
+                ADDI            R4, R4, -96
+                SUB             R4, R0, R4                  ; R4 = hodnota kluca
 keyIfEnd:       ; Zasifrovanie znaku
-                DADD            R10, R10, R4                ; R10 = ASCII znak sifrovaneho znaku
+                ADD             R10, R10, R4                ; R10 = ASCII znak sifrovaneho znaku
 
                 ; Kontrola rozsahu malych pismen (1-26)
                 ; Znak je mensi ako 'a'
                 SLTI            R4, R10, 97                 ; R4 = 0 ak je znak mensi ako 'a', inak R1 = 1
                 BEQZ            R4, noUnderflow
-                DADDI           R10, R10, 26                ; Underflow correction
+                ADDI            R10, R10, 26                ; Underflow correction
+                B               noOverflow
 noUnderflow:    ; Znak je vacsi ako 'z'
                 SLTI            R4, R10, 123                ; R4 = 0 ak je znak vacsi ako 'z', inak R1 = 1
                 BNEZ            R4, noOverflow
-                DADDI           R10, R10, -26               ; Overflow correction
+                ADDI            R10, R10, -26               ; Overflow correction
 noOverflow:     ; Koniec kontroly rozsahu
 
                 ; Vlozenie sifrovaneho znaku do vystupneho retazca
                 SB              R10, cipher(R20)            ; vlozenie sifrovaneho znaku do vystupneho retazca
                 ; Pokracovanie cyklu
-                DADDI           R20, R20, 1                 ; R20 = index++
+                ADDI            R20, R20, 1                 ; R20 = index++
                 B               encryptLoop
 
 encryptEnd:     ; Vypis vystupneho retazca
